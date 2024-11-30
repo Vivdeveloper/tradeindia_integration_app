1b7575c8ccf2e1bb88fcdb850b7c98da518e839b
Sushant <sushantmanjare33@gmail.com>
2024-11-30 14:39:25 +0530

    init

---
+++ tradeindia_integration/tradeindia_integration/doctype/tradeindia_api_settings/test_tradeindia_api_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Exacuer and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTradeIndiaAPISettings(FrappeTestCase):
+	pass
---
+++ tradeindia_integration/tradeindia_integration/doctype/tradeindia_api_settings/tradeindia_api_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Exacuer and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TradeIndia API Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
