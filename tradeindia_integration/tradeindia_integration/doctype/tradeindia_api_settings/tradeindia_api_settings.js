cccc17e72ad7bf399b7598f2735dc20e87ac2d80
Sushant <sushantmanjare33@gmail.com>
2024-12-03 18:41:46 +0530

    changes

--- tradeindia_integration/tradeindia_integration/doctype/tradeindia_api_settings/tradeindia_api_settings.js
+++ tradeindia_integration/tradeindia_integration/doctype/tradeindia_api_settings/tradeindia_api_settings.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Exacuer and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('TradeIndia API Settings', {
+frappe.ui.form.on('Tradeindia API Settings', {
 	// refresh: function(frm) {
 
 	// }
