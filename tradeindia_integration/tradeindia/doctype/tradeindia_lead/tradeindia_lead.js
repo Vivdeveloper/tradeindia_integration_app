57a95f121361acef9c9b967662d98bec5044f0d8
Sushant <sushantmanjare33@gmail.com>
2024-12-11 11:39:36 +0530

    init

---
+++ tradeindia_integration/tradeindia/doctype/tradeindia_lead/tradeindia_lead.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, sushant and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("TradeIndia Lead", {
+// 	refresh(frm) {
+
+// 	},
+// });
